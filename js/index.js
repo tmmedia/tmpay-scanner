3c119515595f91b4a38c61da0ffd82a78b321d7a
tmmedia <tommerkestijn@Ts-MacBook-Pro.local>
2015-06-04 20:36:53 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -34,7 +34,6 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.getElementById('scan').addEventListener('click', this.scan, false);
-        document.getElementById('encode').addEventListener('click', this.encode, false);
     },
 
     // deviceready Event Handler
